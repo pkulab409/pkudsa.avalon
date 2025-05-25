b66f8530cd09f7347e40dee7e98713b88e5fe48a
YouZiliCC <52035934+YouZiliCC@users.noreply.github.com>
2025-05-25 15:43:49 +0800

    装饰器修复

--- retrieval_system/app.py
+++ retrieval_system/app.py
@@ -213,5 +213,4 @@ if __name__ == "__main__":
     init_file_watcher()
     # 注册退出清理
     atexit.register(lambda: observer.stop() and observer.join())
-    
     app.run(port=5050, debug=False, host="0.0.0.0")
